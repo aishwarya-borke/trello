0044608fd794f8889aff8543c59844d2e8b52aae
aishwarya-borke <aishwarya.borke@bankopen.co>
2023-05-05 18:45:23 +0530

    minor changes Added

--- src/components/CardsList.tsx
+++ src/components/CardsList.tsx
@@ -1,22 +1,21 @@
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import { Button, IconButton } from "@mui/material";
+import { Box, Button, ClickAwayListener, IconButton } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import AddCardIcon from "@mui/icons-material/AddCard";
 import { useState } from "react";
 import CloseIcon from "@mui/icons-material/Close";
-import FormControlUnstyled from "@mui/base/FormControlUnstyled";
 import { TextField } from "@mui/material";
 import ListItems from "./ListItems";
 
 // interface props {
-//   title: string;
+//   textarea: string;
 // }
 
 interface cardbox {
   listcard: { list: string }[];
 }
 
-export default function Cards(props: { title: string }) {
+export default function Cards(props: { textarea: string }) {
   const [handlecard, setHandleCard] = useState(false);
   const cardHandler = () => {
     setHandleCard(true);
@@ -30,11 +29,31 @@ export default function Cards(props: { title: string }) {
       setTitle("");
     }
   };
+  const OutsideHandleClick = () => {
+    setHandleCard(false);
+    if (title.trim().length !== 0) {
+      setTitleList([...titlelist, { listcard: [{ list: title }] }]);
+      setTitle("");
+    }
+  }
 
+  const keyEventHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      setTitleList([...titlelist, { listcard: [{ list: title }] }]);
+      setTitle("");
+    }
+  }
+  const textareaEnter = (e:any) => {
+    if (e.key === "Enter"){
+      setTitle(props.textarea);
+      e.preventDefault();
+    }
+  }
+  
   return (
     <div className="bg-white w-72 rounded-xl p-2 mx-2 h-full">
       <div className="flex items-center mx-2 justify-between">
-        <textarea className="resize-none h-6">{props.title}</textarea>
+        <textarea className="resize-none h-6" onDrop={(e) => {e.preventDefault()}} onKeyDown={textareaEnter}>{props.textarea}</textarea>
         <IconButton aria-label="Example">
           <MoreHorizIcon />
         </IconButton>
@@ -52,8 +71,8 @@ export default function Cards(props: { title: string }) {
           draggable?.classList.remove("hide");
         }}
       >
-        {titlelist.map((titlelist) => (
-          <ListItems listcard={titlelist.listcard} />
+        {titlelist.map((titlelist, listcard:any) => (
+          <ListItems key={listcard} listcard={titlelist.listcard} />
         ))}
       </div>
 
@@ -68,14 +87,16 @@ export default function Cards(props: { title: string }) {
       )}
 
       {handlecard && (
-        <div>
-          <FormControlUnstyled className="max-w-none">
+        <ClickAwayListener onClickAway={OutsideHandleClick}>
+          <Box className="max-w-none">
             <TextField
               sx={{ paddingX: "8px", width: "272px" }}
               id="getText"
               placeholder="Enter a title for this card..."
               onChange={(e) => setTitle(e.target.value)}
               value={title}
+              onDrop={(e) => e.preventDefault()}
+              onKeyPress={keyEventHandler}
             />
             <div className="flex justify-between mr-2">
               <div className="flex items-center">
@@ -107,8 +128,8 @@ export default function Cards(props: { title: string }) {
                 <MoreHorizIcon />
               </IconButton>
             </div>
-          </FormControlUnstyled>
-        </div>
+          </Box>
+        </ClickAwayListener>
       )}
     </div>
   );
